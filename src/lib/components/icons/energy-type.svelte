d688f42881ba2db7ab2b6a23e4eca399ed6dae1a
Michael Tichopad <michael@tichopad.dev>
2024-04-01 13:22:17 +0200

    Still on bill creation

---
+++ src/lib/components/icons/energy-type.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import type { EnergyType } from '$lib/models/common';
+	import { cn } from '$lib/utils';
+	import type { ComponentType } from 'svelte';
+	import DropletsIcon from './droplets.svelte';
+	import FlameIcon from './flame.svelte';
+	import LightbulbIcon from './lightbulb.svelte';
+
+	export let energyType: EnergyType;
+
+	const iconByEnergyType = {
+		electricity: LightbulbIcon,
+		water: DropletsIcon,
+		heating: FlameIcon
+	} as const satisfies Record<EnergyType, ComponentType>;
+</script>
+
+<svelte:component
+	this={iconByEnergyType[energyType]}
+	class={cn(
+		{
+			'text-yellow-500': energyType === 'electricity',
+			'text-blue-600': energyType === 'water',
+			'text-red-600': energyType === 'heating'
+		},
+		$$props.class
+	)}
+/>
--- src/routes/bills/create/+page.svelte
+++ src/routes/bills/create/+page.svelte
@@ -5,7 +5,7 @@
 	import * as Popover from '$lib/components/ui/popover';
 	import { cn } from '$lib/utils';
 	import { Button, buttonVariants } from '$lib/components/ui/button';
-	import CalendarIcon from 'svelte-radix/Calendar.svelte';
+	import { Calendar as CalendarIcon, Person as PersonIcon } from 'svelte-radix';
 	import { RangeCalendar } from '$lib/components/ui/range-calendar';
 	import {
 		today,
@@ -15,12 +15,14 @@
 		type DateValue
 	} from '@internationalized/date';
 	import type { DateRange } from 'bits-ui';
-	import Droplets from '$lib/components/icons/droplets.svelte';
+	import DropletsIcon from '$lib/components/icons/droplets.svelte';
 	import * as Card from '$lib/components/ui/card';
 	import * as Table from '$lib/components/ui/table';
 	import { measuringDevices } from '$lib/models/measuring-device';
 	import { labelsByEnergyType, unitsByEnergyType } from '$lib/models/common';
-	import Lightbulb from '$lib/components/icons/lightbulb.svelte';
+	import LightbulbIcon from '$lib/components/icons/lightbulb.svelte';
+	import FlameIcon from '$lib/components/icons/flame.svelte';
+	import EnergyTypeIcon from '$lib/components/icons/energy-type.svelte';
 
 	export let data;
 
@@ -65,197 +67,219 @@
 	class="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 p-4 md:gap-8 md:p-10"
 >
 	<div>
-		<h1 class="text-2xl font-bold py-2">Create new bill</h1>
+		<h1 class="text-2xl font-bold">Create new bill</h1>
 	</div>
 	<div>
 		<form method="post" action="?/createBill" use:enhance class="flex flex-col gap-2">
-			<fieldset class="rounded-lg border p-4">
-				<legend class="px-1 text-sm font-medium"> Bill info </legend>
-				<!-- Date range -->
-				<Form.Field {form} name="dateRange" class="flex flex-col">
-					<Form.Control let:attrs>
-						<Form.Label>Date range</Form.Label>
-						<Popover.Root openFocus>
-							<Popover.Trigger asChild let:builder>
-								<Button
-									builders={[builder]}
-									class={cn(
-										'w-[300px] justify-start text-left font-normal',
-										!dateRange && 'text-muted-foreground'
-									)}
-									variant="outline"
-								>
-									<CalendarIcon class="mr-2 h-4 w-4" />
-									{#if dateRange && dateRange.start}
-										{#if dateRange.end}
-											{df.format(dateRange.start.toDate(getLocalTimeZone()))} - {df.format(
-												dateRange.end.toDate(getLocalTimeZone())
-											)}
+			<h2 class="text-xl py-2 font-medium">Basic</h2>
+			<div class="grid lg:grid-cols-2 gap-4">
+				<!-- Bill info -->
+				<fieldset class="rounded-lg border p-4">
+					<!-- Date range -->
+					<legend class="px-1 text-sm font-medium"> Bill info </legend>
+					<Form.Field {form} name="dateRange" class="flex flex-col">
+						<Form.Control let:attrs>
+							<Form.Label>Date range</Form.Label>
+							<Popover.Root openFocus>
+								<Popover.Trigger asChild let:builder>
+									<Button
+										builders={[builder]}
+										class={cn(
+											'w-[300px] justify-start text-left font-normal',
+											!dateRange && 'text-muted-foreground'
+										)}
+										variant="outline"
+									>
+										<CalendarIcon class="mr-2 h-4 w-4" />
+										{#if dateRange && dateRange.start}
+											{#if dateRange.end}
+												{df.format(dateRange.start.toDate(getLocalTimeZone()))} - {df.format(
+													dateRange.end.toDate(getLocalTimeZone())
+												)}
+											{:else}
+												{df.format(dateRange.start.toDate(getLocalTimeZone()))}
+											{/if}
+										{:else if startDateValue}
+											{df.format(startDateValue.toDate(getLocalTimeZone()))}
 										{:else}
-											{df.format(dateRange.start.toDate(getLocalTimeZone()))}
+											Pick a date
 										{/if}
-									{:else if startDateValue}
-										{df.format(startDateValue.toDate(getLocalTimeZone()))}
-									{:else}
-										Pick a date
-									{/if}
-								</Button>
-							</Popover.Trigger>
-							<Popover.Content class="w-auto p-0" align="start">
-								<RangeCalendar
-									bind:startValue={startDateValue}
-									bind:value={dateRange}
-									class="rounded-md border shadow"
-									initialFocus
-									numberOfMonths={3}
-								/>
-							</Popover.Content>
-						</Popover.Root>
-						<Form.Description>How long how looong</Form.Description>
-						<Form.FieldErrors />
-						<input hidden {...attrs} value={$formData.dateRange.start} name="dateRange.start" />
-						<input hidden {...attrs} value={$formData.dateRange.end} name="dateRange.end" />
-					</Form.Control>
-				</Form.Field>
-			</fieldset>
-			<fieldset class="rounded-lg border p-4">
-				<legend class="px-1 text-sm font-medium"> Total energy consumption and cost </legend>
-				<Table.Root>
-					<Table.Header>
-						<Table.Row>
-							<Table.Head class="w-[100px]">Energy type</Table.Head>
-							<Table.Head class="w-[200px]">Total cost</Table.Head>
-							<Table.Head class="w-[200px]">Fixed cost</Table.Head>
-							<Table.Head class="w-[100px]">Unit</Table.Head>
-							<Table.Head class="w-[200px]">Total consumption</Table.Head>
-							<Table.Head class="w-[100px]">Unit</Table.Head>
-						</Table.Row>
-					</Table.Header>
-					<Table.Body>
-						<!-- Electricity -->
-						<Table.Row>
-							<Table.Cell>{labelsByEnergyType['electricity']}</Table.Cell>
-							<Table.Cell>
-								<Form.Field {form} name="electricityTotalCost">
-									<Form.Control let:attrs>
-										<Input
-											type="number"
-											step="0.001"
-											placeholder="0.000"
-											{...attrs}
-											bind:value={$formData.electricityTotalCost}
-										/>
-									</Form.Control>
-								</Form.Field>
-							</Table.Cell>
-							<Table.Cell><Input type="number" disabled /></Table.Cell>
-							<Table.Cell>CZK</Table.Cell>
-							<Table.Cell>
-								<Form.Field {form} name="electricityTotalConsumption">
-									<Form.Control let:attrs>
-										<Input
-											type="number"
-											step="0.001"
-											placeholder="0.000"
-											{...attrs}
-											bind:value={$formData.electricityTotalConsumption}
-										/>
-									</Form.Control>
-								</Form.Field>
-							</Table.Cell>
-							<Table.Cell>{unitsByEnergyType['electricity']}</Table.Cell>
-						</Table.Row>
-						<!-- Water -->
-						<Table.Row>
-							<Table.Cell>{labelsByEnergyType['water']}</Table.Cell>
-							<Table.Cell>
-								<Form.Field {form} name="waterTotalCost">
-									<Form.Control let:attrs>
-										<Input
-											type="number"
-											step="0.001"
-											placeholder="0.000"
-											{...attrs}
-											bind:value={$formData.waterTotalCost}
-										/>
-									</Form.Control>
-								</Form.Field>
-							</Table.Cell>
-							<Table.Cell><Input type="number" disabled /></Table.Cell>
-							<Table.Cell>CZK</Table.Cell>
-							<Table.Cell>
-								<Form.Field {form} name="waterTotalConsumption">
-									<Form.Control let:attrs>
-										<Input
-											type="number"
-											step="0.001"
-											placeholder="0.000"
-											{...attrs}
-											bind:value={$formData.waterTotalConsumption}
-										/>
-									</Form.Control>
-								</Form.Field>
-							</Table.Cell>
-							<Table.Cell>{unitsByEnergyType['water']}</Table.Cell>
-						</Table.Row>
-						<!-- Heating -->
-						<Table.Row>
-							<Table.Cell>{labelsByEnergyType['heating']}</Table.Cell>
-							<Table.Cell>
-								<Form.Field {form} name="heatingTotalCost">
-									<Form.Control let:attrs>
-										<Input
-											type="number"
-											step="0.001"
-											placeholder="0.000"
-											{...attrs}
-											bind:value={$formData.heatingTotalCost}
-										/>
-									</Form.Control>
-								</Form.Field>
-							</Table.Cell>
-							<Table.Cell>
-								<Form.Field {form} name="heatingTotalFixedCost">
-									<Form.Control let:attrs>
-										<Input
-											type="number"
-											step="0.001"
-											placeholder="0.000"
-											{...attrs}
-											bind:value={$formData.heatingTotalFixedCost}
-										/>
-									</Form.Control>
-								</Form.Field>
-							</Table.Cell>
-							<Table.Cell>CZK</Table.Cell>
-							<Table.Cell>
-								<Form.Field {form} name="heatingTotalConsumption">
-									<Form.Control let:attrs>
-										<Input
-											type="number"
-											step="0.001"
-											placeholder="0.000"
-											{...attrs}
-											bind:value={$formData.heatingTotalConsumption}
-										/>
-									</Form.Control>
-								</Form.Field>
-							</Table.Cell>
-							<Table.Cell>{unitsByEnergyType['heating']}</Table.Cell>
-						</Table.Row>
-					</Table.Body>
-				</Table.Root>
-			</fieldset>
+									</Button>
+								</Popover.Trigger>
+								<Popover.Content class="w-auto p-0" align="start">
+									<RangeCalendar
+										bind:startValue={startDateValue}
+										bind:value={dateRange}
+										class="rounded-md border shadow"
+										initialFocus
+										numberOfMonths={3}
+									/>
+								</Popover.Content>
+							</Popover.Root>
+							<Form.Description>How long how looong</Form.Description>
+							<Form.FieldErrors />
+							<input hidden {...attrs} value={$formData.dateRange.start} name="dateRange.start" />
+							<input hidden {...attrs} value={$formData.dateRange.end} name="dateRange.end" />
+						</Form.Control>
+					</Form.Field>
+				</fieldset>
+				<!-- Energies -->
+				<fieldset class="rounded-lg border p-4">
+					<legend class="px-1 text-sm font-medium"> Total energy consumption and cost </legend>
+					<Table.Root>
+						<Table.Header>
+							<Table.Row>
+								<Table.Head class="w-[100px]">Energy type</Table.Head>
+								<Table.Head class="w-[200px]">Total cost</Table.Head>
+								<Table.Head class="w-[200px]">Fixed cost</Table.Head>
+								<Table.Head class="w-[100px]">Unit</Table.Head>
+								<Table.Head class="w-[200px]">Total consumption</Table.Head>
+								<Table.Head class="w-[100px]">Unit</Table.Head>
+							</Table.Row>
+						</Table.Header>
+						<Table.Body>
+							<!-- Electricity -->
+							<Table.Row>
+								<Table.Cell>
+									<div class="flex items-center gap-1 font-medium">
+										<LightbulbIcon class="w-4 h-4 text-yellow-500" />
+										<span>{labelsByEnergyType['electricity']}</span>
+									</div>
+								</Table.Cell>
+								<Table.Cell>
+									<Form.Field {form} name="electricityTotalCost">
+										<Form.Control let:attrs>
+											<Input
+												type="number"
+												step="0.001"
+												placeholder="0.000"
+												{...attrs}
+												bind:value={$formData.electricityTotalCost}
+											/>
+										</Form.Control>
+									</Form.Field>
+								</Table.Cell>
+								<Table.Cell><Input type="number" disabled /></Table.Cell>
+								<Table.Cell>CZK</Table.Cell>
+								<Table.Cell>
+									<Form.Field {form} name="electricityTotalConsumption">
+										<Form.Control let:attrs>
+											<Input
+												type="number"
+												step="0.001"
+												placeholder="0.000"
+												{...attrs}
+												bind:value={$formData.electricityTotalConsumption}
+											/>
+										</Form.Control>
+									</Form.Field>
+								</Table.Cell>
+								<Table.Cell>{unitsByEnergyType['electricity']}</Table.Cell>
+							</Table.Row>
+							<!-- Water -->
+							<Table.Row>
+								<Table.Cell>
+									<div class="flex items-center gap-1 font-medium">
+										<DropletsIcon class="w-4 h-4 text-blue-600" />
+										<span>{labelsByEnergyType['water']}</span>
+									</div>
+								</Table.Cell>
+								<Table.Cell>
+									<Form.Field {form} name="waterTotalCost">
+										<Form.Control let:attrs>
+											<Input
+												type="number"
+												step="0.001"
+												placeholder="0.000"
+												{...attrs}
+												bind:value={$formData.waterTotalCost}
+											/>
+										</Form.Control>
+									</Form.Field>
+								</Table.Cell>
+								<Table.Cell><Input type="number" disabled /></Table.Cell>
+								<Table.Cell>CZK</Table.Cell>
+								<Table.Cell>
+									<Form.Field {form} name="waterTotalConsumption">
+										<Form.Control let:attrs>
+											<Input
+												type="number"
+												step="0.001"
+												placeholder="0.000"
+												{...attrs}
+												bind:value={$formData.waterTotalConsumption}
+											/>
+										</Form.Control>
+									</Form.Field>
+								</Table.Cell>
+								<Table.Cell>{unitsByEnergyType['water']}</Table.Cell>
+							</Table.Row>
+							<!-- Heating -->
+							<Table.Row>
+								<Table.Cell>
+									<div class="flex items-center gap-1 font-medium">
+										<FlameIcon class="w-4 h-4 text-red-600" />
+										<span>{labelsByEnergyType['heating']}</span>
+									</div>
+								</Table.Cell>
+								<Table.Cell>
+									<Form.Field {form} name="heatingTotalCost">
+										<Form.Control let:attrs>
+											<Input
+												type="number"
+												step="0.001"
+												placeholder="0.000"
+												{...attrs}
+												bind:value={$formData.heatingTotalCost}
+											/>
+										</Form.Control>
+									</Form.Field>
+								</Table.Cell>
+								<Table.Cell>
+									<Form.Field {form} name="heatingTotalFixedCost">
+										<Form.Control let:attrs>
+											<Input
+												type="number"
+												step="0.001"
+												placeholder="0.000"
+												{...attrs}
+												bind:value={$formData.heatingTotalFixedCost}
+											/>
+										</Form.Control>
+									</Form.Field>
+								</Table.Cell>
+								<Table.Cell>CZK</Table.Cell>
+								<Table.Cell>
+									<Form.Field {form} name="heatingTotalConsumption">
+										<Form.Control let:attrs>
+											<Input
+												type="number"
+												step="0.001"
+												placeholder="0.000"
+												{...attrs}
+												bind:value={$formData.heatingTotalConsumption}
+											/>
+										</Form.Control>
+									</Form.Field>
+								</Table.Cell>
+								<Table.Cell>{unitsByEnergyType['heating']}</Table.Cell>
+							</Table.Row>
+						</Table.Body>
+					</Table.Root>
+				</fieldset>
+			</div>
 
 			<!-- Occupants -->
-			<Form.Fieldset {form} class="rounded-lg border p-4 flex flex-col" name="occupants">
-				<Form.Legend class="px-1 text-sm font-medium">Occupants</Form.Legend>
-
+			<Form.Fieldset {form} class="grid lg:grid-cols-2 gap-4" name="occupants">
+				<Form.Legend class="text-xl py-2 font-medium">Occupants</Form.Legend>
 				<!-- New occupants -->
 				{#each $formData.occupants as occupant, i (occupant.id)}
 					<Card.Root>
 						<Card.Header>
-							<Card.Title>{occupant.name}</Card.Title>
+							<Card.Title class="flex items-center gap-1">
+								<PersonIcon class="w-4 h-6 text-muted-foreground" />
+								<span>{occupant.name}</span>
+							</Card.Title>
 							<Card.Description>
 								<div class="grid gap-2 pt-2">
 									<div class="font-semibold">Occupant Information</div>
@@ -298,6 +322,7 @@
 							</Card.Description>
 						</Card.Header>
 						<Card.Content>
+							<!-- Measuring devices -->
 							{#if occupant.measuringDevices.length > 0}
 								<Form.Fieldset {form} name="occupants[{i}].measuringDevices">
 									<Form.Legend class="text-sm font-medium">Measuring devices</Form.Legend>
@@ -313,9 +338,12 @@
 										<Table.Body>
 											{#each occupant.measuringDevices as device, j (device.id)}
 												<Table.Row>
-													<Table.Cell class="font-semibold"
-														>{labelsByEnergyType[device.energyType]}</Table.Cell
-													>
+													<Table.Cell class="font-medium">
+														<div class="flex items-center gap-1">
+															<EnergyTypeIcon class="w-4 h-4" energyType={device.energyType} />
+															<span>{labelsByEnergyType[device.energyType]}</span>
+														</div>
+													</Table.Cell>
 													<Table.Cell>{device.name}</Table.Cell>
 													<Table.Cell>
 														<input type="hidden" bind:value={device.id} />
@@ -341,43 +369,8 @@
 						</Card.Content>
 					</Card.Root>
 				{/each}
-
-				<!-- Old occupants -->
-				<!-- <Form.Fieldset {form} name="occupants">
-					<Form.Legend class="text-xl">Occupants</Form.Legend>
-					{#each $formData.occupants as occupant, i}
-						<Form.ElementField {form} name="occupants[{i}]">
-							<input type="hidden" bind:value={occupant.id} />
-							{#if occupant.measuringDevices.length > 0}
-								<Form.Fieldset {form} class="px-4" name="occupants[{i}].measuringDevices">
-									<Form.Legend class="text-lg">Measuring Devices</Form.Legend>
-									{#each occupant.measuringDevices as device, j}
-										<Form.ElementField
-											{form}
-											class="px-4"
-											name="occupants[{i}].measuringDevices[{j}]"
-										>
-											<Form.Control let:attrs>
-												<input type="hidden" bind:value={device.id} {...attrs} />
-											</Form.Control>
-											<Form.Control let:attrs>
-												<Form.Label>{device.name} ({device.energyType})</Form.Label>
-												<Input
-													type="number"
-													step="0.001"
-													{...attrs}
-													bind:value={device.consumption}
-												/>
-											</Form.Control>
-										</Form.ElementField>
-									{/each}
-								</Form.Fieldset>
-							{/if}
-						</Form.ElementField>
-					{/each}
-				</Form.Fieldset> -->
 			</Form.Fieldset>
-			<Form.Button>Save</Form.Button>
+			<Form.Button class="sm:max-w-20">Save</Form.Button>
 		</form>
 	</div>
 </main>
